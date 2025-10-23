9e7159fb4e7e1c84052cf4130c23d843e28a04cf
devkaahiye <developerkaahiye@gmail.com>
2025-10-23 08:17:49 +0300

    changes on backend side

--- server/index.js
+++ server/index.js
@@ -2,12 +2,14 @@ import express, { Router } from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import userRoutes from './routes/userRouts.js'
+import cors from 'cors'
 const app = express()
 
 dotenv.config()
 
 
 app.use(express.json())
+app.use(cors())
 app.use('/api', userRoutes);
 
 
